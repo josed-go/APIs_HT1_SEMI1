66d82809fb5af6b912d4ed6138b92ee9be069430
EspinozaJolon01 <3524722860101@ingenieria.usac.edu.gt>
2025-08-06 20:25:08 -0600

    modificacion api2

--- maquina2/index.py
+++ maquina2/index.py
@@ -4,10 +4,8 @@ import os
 
 app = Flask(__name__)
 
-# Ruta al archivo JSON (ajusta la ruta según donde lo guardes en la instancia EC2)
 JSON_FILE = os.path.join(os.path.dirname(__file__), "instance_info.json")
 
-# Cargar datos del archivo JSON al iniciar la aplicación
 with open(JSON_FILE, 'r') as file:
     instance_data = json.load(file)
 
